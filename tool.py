025e054a0b40db78470cdabed84d3cb5ca784bd3
guchunli <1912029196@qq.com>
2018-10-10 16:07:04 +0800

    add photos

--- tool.py
+++ tool.py
@@ -114,10 +114,10 @@ def compress(choose, des_dir, src_dir, file_list):
 
 def handle_photo():
     '''根据图片的文件名处理成需要的json格式的数据
-    
-    -----------
-    最后将data.json文件存到博客的source/photos文件夹下
-    '''
+        
+        -----------
+        最后将data.json文件存到博客的source/photos文件夹下
+        '''
     src_dir, des_dir = "photos/", "min_photos/"
     file_list = list_img_file(src_dir)
     list_info = []
@@ -126,24 +126,24 @@ def handle_photo():
         date_str, info = filename.split("_")
         info, _ = info.split(".")
         date = datetime.strptime(date_str, "%Y-%m-%d")
-        year_month = date_str[0:7]            
+        year_month = date_str[0:7]
         if i == 0:  # 处理第一个文件
             new_dict = {"date": year_month, "arr":{'year': date.year,
-                                                                   'month': date.month,
-                                                                   'link': [filename],
-                                                                   'text': [info],
-                                                                   'type': ['image']
-                                                                   }
-                                        } 
+                'month': date.month,
+                    'link': [filename],
+                        'text': [info],
+                            'type': ['image']
+                            }
+                                }
             list_info.append(new_dict)
         elif year_month != list_info[-1]['date']:  # 不是最后的一个日期，就新建一个dict
             new_dict = {"date": year_month, "arr":{'year': date.year,
-                                                   'month': date.month,
-                                                   'link': [filename],
-                                                   'text': [info],
-                                                   'type': ['image']
-                                                   }
-                        }
+                'month': date.month,
+                    'link': [filename],
+                        'text': [info],
+                            'type': ['image']
+                            }
+                    }
             list_info.append(new_dict)
         else:  # 同一个日期
             list_info[-1]['arr']['link'].append(filename)
@@ -153,6 +153,7 @@ def handle_photo():
     final_dict = {"list": list_info}
     with open("./data.json","w") as fp:
         json.dump(final_dict, fp)
+
 
 def cut_photo():
     """裁剪算法
