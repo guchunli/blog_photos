1e48ae6df28e497d15e4080ff9b8e55c3522ad7d
guchunli <1912029196@qq.com>
2018-10-10 16:02:16 +0800

    add photos

--- tool.py
+++ tool.py
@@ -126,7 +126,7 @@ def handle_photo():
         date_str, info = filename.split("_")
         info, _ = info.split(".")
         date = datetime.strptime(date_str, "%Y-%m-%d")
-        year_month = date_str[0:8]            
+        year_month = date_str[0:7]            
         if i == 0:  # 处理第一个文件
             new_dict = {"date": year_month, "arr":{'year': date.year,
                                                                    'month': date.month,
